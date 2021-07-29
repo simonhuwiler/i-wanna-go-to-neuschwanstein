f4f2ebd4d309116b7f613875f60cc6913faf4406
Simon Huwiler <webmaster@simonhuwiler.ch>
2021-07-29 13:16:42 +0200

    add date

--- src/main.py
+++ src/main.py
@@ -7,8 +7,10 @@ def notify(title, text):
 """
 
 # Add your checks
+check_for_free_tickets("07.08.2021", 2, CASTLES['Hohenschwangnau'], push)
 check_for_free_tickets("08.08.2021", 2, CASTLES['Hohenschwangnau'], push)
 check_for_free_tickets("09.08.2021", 2, CASTLES['Hohenschwangnau'], push)
+check_for_free_tickets("07.08.2021", 2, CASTLES['Neuschwanstein'], push)
 check_for_free_tickets("08.08.2021", 2, CASTLES['Neuschwanstein'], push)
 check_for_free_tickets("09.08.2021", 2, CASTLES['Neuschwanstein'], push)
 
